6c96370fb659a162e4ff2ca3018308b9459ae6b5
jubair19 <122038232+jubair19@users.noreply.github.com>
2025-05-18 14:08:22 +0000

    add variable

---
+++ 01/variable.js
@@ -0,0 +1,13 @@
+const accountId = 144554
+let accountEmail = "jubairdotcom"
+var accountPass = "12345"
+accountCity = "Dhaka"
+let a
+
+// accountId = 2    not allowed
+accountEmail = "abcdotcom"
+accountPass = "1233"
+accountCity = "Ctg"
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity,a])
