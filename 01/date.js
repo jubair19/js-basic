82612c67e48f06733795b464b706734ca205ed27
jubair19 <122038232+jubair19@users.noreply.github.com>
2025-05-19 10:50:19 +0000

    pushing date

---
+++ 01/date.js
@@ -0,0 +1,33 @@
+// let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleString())
+
+// let myCreatedDate = new Date(2023, 9, 19)
+// console.log(myCreatedDate)
+// console.log(myCreatedDate.toDateString())
+// let myCreatedDateT = new Date(2023, 9, 19, 5, 3)
+// console.log(myCreatedDateT.toLocaleString())
+
+// let date1 = new Date("2024-06-01")
+// console.log(date1.toLocaleString())
+// let date2 = new Date("19-05-2025")
+// console.log(date1.toLocaleString())
+
+// let myTime = Date.now()
+// console.log(myTime)
+
+// let date = new Date("01-01-2001")
+// console.log(date.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+// let newD = new Date()
+// console.log(newD.getDate())
+// console.log(newD.getDay())
+// console.log(newD.getMonth() + 1) // Cause jan = 0
+
+
