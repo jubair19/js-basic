415d1f1fee0b2d6f8b054a40ac6157cab591432b
jubair19 <122038232+jubair19@users.noreply.github.com>
2025-05-19 07:27:08 +0000

    push math, string

---
+++ 01/math.js
@@ -0,0 +1,37 @@
+// const number  = 400
+// console.log(number)
+
+// const balance = new Number(300)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const othNum = 1223.7899
+// console.log(othNum.toPrecision(8));
+// console.log(othNum.toPrecision(4));
+// console.log(othNum.toPrecision(5));
+
+// const hubn = 10000000
+// console.log(hubn.toLocaleString())
+// console.log(hubn.toExponential())
+
+// ++++++++ Math ++++++
+
+// console.log(Math);
+// console.log(Math.abs(-65));
+// console.log(Math.round(5.5));
+// console.log(Math.round(5.4));
+// console.log(Math.sqrt(5.4));
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor((Math.random()*10)) + 1)
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
---
+++ 01/string.js
@@ -0,0 +1,23 @@
+const name = "jubair"
+const num = 50
+
+// console.log(name + num +"!"); Dont use that
+
+// console.log(`hello this is ${name}, i have ${num} taka`)
+
+const game = new String('Jubair al sadid')
+
+// console.log(game[0])
+// console.log(game.__proto__)
+// console.log(game.length)
+// console.log(game.toUpperCase())
+// console.log(game.charAt(2))
+// console.log(game.indexOf('t'))
+
+// const newgame = game.substring(1,3)
+// console.log(newgame);
+
+// console.log(game.replace(' ','-'))
+// console.log(game.replaceAll(' ','-'))
+
+
